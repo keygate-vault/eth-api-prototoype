b597a97840c399a38a23c2b3151c8b0506032de2
Joaquin Ballara <quino@192.168.1.3>
2024-10-22 10:15:49 -0300

    getbalance test

--- src/eth_backend/src/lib.rs
+++ src/eth_backend/src/lib.rs
@@ -3,52 +3,20 @@ use ethers_core;
 use ic_cdk::api::management_canister::http_request::HttpHeader;
 use serde::{Deserialize, Serialize};
 
+use alloy::{
+    network::TxSigner,
+    primitives::Address,
+    providers::{Provider, ProviderBuilder},
+    signers::icp::IcpSigner,
+    transports::icp::{EthSepoliaService, IcpConfig, RpcApi, RpcService},
+};
+
 #[cfg(test)]
 mod test;
 
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
-
-#[derive(Clone, PartialEq, Eq, Ord, PartialOrd, Hash, Serialize, Deserialize, CandidType)]
-struct RpcApi {
-    pub url: String,
-    pub headers: Option<Vec<HttpHeader>>,
-}
-
-#[derive(
-    Clone, Copy, Debug, PartialEq, Eq, Ord, PartialOrd, Hash, Serialize, Deserialize, CandidType,
-)]
-enum EthMainnetService {
-    Alchemy,
-    Ankr,
-    BlockPi,
-    PublicNode,
-    Cloudflare,
-}
-
-#[derive(
-    Clone, Copy, Debug, PartialEq, Eq, Ord, PartialOrd, Hash, Serialize, Deserialize, CandidType,
-)]
-enum EthSepoliaService {
-    Alchemy,
-    Ankr,
-    BlockPi,
-    PublicNode,
-}
-
-#[derive(Clone, PartialEq, Eq, Ord, PartialOrd, Hash, Serialize, Deserialize, CandidType)]
-enum RpcService {
-    EthMainnet(EthMainnetService),
-    EthSepolia(EthSepoliaService),
-    Chain(u64),
-    Provider(u64),
-    Custom(RpcApi),
-}
 static RPC_SERVICE: RpcService = RpcService::EthSepolia(EthSepoliaService::PublicNode);
 
-#[derive(CandidType, Serialize, Deserialize, Clone)]
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
 struct TransactionRequest {
     to: String,
     value: u64,
@@ -115,7 +83,10 @@ async fn pubkey_bytes_to_address() -> String {
 async fn get_public_key() -> Result<PublicKeyReply, String> {
     let key_id = EcdsaKeyId {
         curve: EcdsaCurve::Secp256k1,
+        #[cfg(not(test))]
         name: "dfx_test_key".to_string(),
+        #[cfg(test)]
+        name: "dfx_test_key1".to_string(),
     };
     let ic_canister_id = "aaaaa-aa";
     let ic = Principal::from_text(&ic_canister_id).unwrap();
@@ -137,16 +108,17 @@ async fn get_public_key() -> Result<PublicKeyReply, String> {
 
 #[ic_cdk::update]
 async fn execute_transaction(request: TransactionRequest) -> TransactionResult {
+    ic_cdk::println!("Executing transaction: {:?}", request);
     let payload = format!(
         r#"{{"jsonrpc":"2.0","method":"eth_getBalance","params":["{}","latest"],"id":1}}"#,
         pubkey_bytes_to_address().await
     );
     const maxResponseSize: u64 = 1000;
-    let canisterId = Principal::from_text("aaaaa-aa").unwrap();
+    let canister_id = Principal::from_text("aaaaa-aa").unwrap();
     let params = (&RPC_SERVICE, payload, maxResponseSize);
 
     let (cycles_result,): (Result<u128, String>,) =
-        ic_cdk::api::call::call(canisterId, "requestCost", params.clone())
+        ic_cdk::api::call::call(canister_id, "requestCost", params.clone())
             .await
             .unwrap();
 
@@ -154,7 +126,7 @@ async fn execute_transaction(request: TransactionRequest) -> TransactionResult {
         .unwrap_or_else(|e| ic_cdk::trap(&format!("error in `request_cost`: {:?}", e)));
 
     let (result,): (Result<String, String>,) =
-        ic_cdk::api::call::call_with_payment128(canisterId, "request", params, cycles)
+        ic_cdk::api::call::call_with_payment128(canister_id, "request", params, cycles)
             .await
             .unwrap();
 
@@ -178,38 +150,15 @@ async fn execute_transaction(request: TransactionRequest) -> TransactionResult {
 }
 
 #[ic_cdk::update]
-async fn get_balance() -> u128 {
-    let payload = format!(
-        r#"{{"jsonrpc":"2.0","method":"eth_getBalance","params":["{}","latest"],"id":1}}"#,
-        pubkey_bytes_to_address().await
-    );
-    const maxResponseSize: u64 = 1000;
-    let canisterId = Principal::from_text("7hfb6-caaaa-aaaar-qadga-cai").unwrap();
-    let params = (&RPC_SERVICE, payload, maxResponseSize);
-
-    let (cycles_result,): (Result<u128, String>,) =
-        ic_cdk::api::call::call(canisterId, "requestCost", params.clone())
-            .await
-            .unwrap();
-
-    let cycles = cycles_result
-        .unwrap_or_else(|e| ic_cdk::trap(&format!("error in `request_cost`: {:?}", e)));
-
-    let (result,): (Result<String, String>,) =
-        ic_cdk::api::call::call_with_payment128(canisterId, "request", params, cycles)
-            .await
-            .unwrap();
-
-    ic_cdk::println!("RPC result: {:?}", result);
+async fn get_balance() -> Result<String, String> {
+    let address = pubkey_bytes_to_address().await;
+    let address = address.parse::<Address>().map_err(|e| e.to_string())?;
+    let config = IcpConfig::new(RPC_SERVICE.clone());
+    let provider = ProviderBuilder::new().on_icp(config);
+    let result = provider.get_balance(address).await;
 
     match result {
-        Ok(response) => match u128::from_str_radix(&response[36..response.len() - 2], 16) {
-            Ok(balance) => balance,
-            Err(e) => ic_cdk::trap(&format!(
-                "error parsing balance from response: {:?}, response: {:?}",
-                e, response
-            )),
-        },
-        Err(err) => ic_cdk::trap(&format!("error in `request` with cycles: {:?}", err)),
+        Ok(balance) => Ok(balance.to_string()),
+        Err(e) => Err(e.to_string()),
     }
 }
--- src/eth_backend/src/test.rs
+++ src/eth_backend/src/test.rs
@@ -1,16 +1,27 @@
 #[cfg(test)]
 use candid::{CandidType, Principal};
 mod tests {
-    use pocket_ic::{query_candid, update_candid, CallError, PocketIc};
+    use candid::encode_one;
+    use ethers_core::abi::encode;
+    use pocket_ic::{query_candid, update_candid, CallError, PocketIc, PocketIcBuilder};
+    use serde::{Deserialize, Serialize};
 
     use crate::{TransactionRequest, TransactionResult};
 
     use super::*;
 
+    #[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
+    struct InitArgs {
+        nodesInSubnet: u32,
+    }
+
     #[test]
     fn it_works() {
         // Create test state
-        let pic = PocketIc::new();
+        let pic = PocketIcBuilder::new()
+            .with_ii_subnet() // to have tECDSA keys available
+            .with_application_subnet()
+            .build();
         let canister_id = pic.create_canister();
         pic.add_cycles(canister_id, 2_000_000_000_000);
 
@@ -22,24 +33,28 @@ mod tests {
         let rpc_canister_id = pic.create_canister();
         pic.add_cycles(rpc_canister_id, 2_000_000_000_000);
         let evm_rpc_wasm_module = include_bytes!("./evm_rpc.wasm").to_vec();
-        pic.install_canister(rpc_canister_id, evm_rpc_wasm_module, Vec::new(), None);
+        pic.install_canister(
+            rpc_canister_id,
+            evm_rpc_wasm_module,
+            encode_one((InitArgs { nodesInSubnet: 28 },)).unwrap(),
+            None,
+        );
 
         let payload = TransactionRequest {
             to: "0x1234567890".to_string(),
             value: 100,
         };
-        let payloadTo = "0x1234567890".to_string();
+        let payload_to = "0x1234567890".to_string();
 
         // Execute test functions or methods
         let res: (TransactionResult,) =
             update_candid(&pic, canister_id, "execute_transaction", (payload,)).unwrap();
         // Query RPC to get ether balance for "to" address
         let res: (u128, String, String) =
-            query_candid(&pic, rpc_canister_id, "eth_getBalance", (payloadTo,)).unwrap();
-
+            query_candid(&pic, canister_id, "get_balance", ()).unwrap();
         // Assert that the transaction was successful
         // Assert that the ether balance for "to" address is equal to the value of the transaction
-        assert_eq!(res.0, 100);
+        assert_eq!(100, res.0);
         assert!(true);
     }
 }
